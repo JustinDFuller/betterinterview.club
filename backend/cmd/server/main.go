e488df5ab9f51fa3bfaeb5f7df7a01d667b0cc77
Justin Fuller <justindanielfuller@gmail.com>
2022-10-04 20:45:18 -0400

    Statically serve some files

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello World")
-	})
+	http.Handle("/", http.FileServer(http.Dir("./")))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ backend/types.go
@@ -0,0 +1,12 @@
+package interview
+
+type Organization struct {
+	ID          string
+	Name        string
+	EmailDomain string
+}
+
+type Person struct {
+	ID    string
+	Email string
+}
