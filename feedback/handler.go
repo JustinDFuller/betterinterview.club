265204cb8e76aa113f7ceff5f1a331e57e17bf23
Justin Fuller <justindanielfuller@gmail.com>
2022-10-20 09:06:05 -0400

    Gathering feedback

--- feedback/handler.go
+++ feedback/handler.go
@@ -1,33 +1,105 @@
 package feedback
 
 import (
+	"io"
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/justindfuller/interviews/organization"
 )
 
 func Handler(organizations *organization.Organizations) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie("__Host-UserUUID")
-		if err != nil {
-			log.Printf("Error parsing cookie for /feedback: %s", err)
-			http.ServeFile(w, r, "./error/unauthenticated.html")
+		if r.Method == http.MethodGet {
+			cookie, err := r.Cookie("__Host-UserUUID")
+			if err != nil {
+				log.Printf("Error parsing cookie for /feedback: %s", err)
+				http.ServeFile(w, r, "./error/unauthenticated.html")
+				return
+			}
+
+			if cookie.Value == "" {
+				log.Printf("Error parsing cookie for /feedback: %s", err)
+				http.ServeFile(w, r, "./error/unauthenticated.html")
+				return
+			}
+
+			if _, err := organizations.FindByUserID(cookie.Value); err != nil {
+				log.Printf("Error finding organization for /feedback: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			http.ServeFile(w, r, "./feedback/index.html")
+
 			return
 		}
 
-		if cookie.Value == "" {
-			log.Printf("Error parsing cookie for /feedback: %s", err)
-			http.ServeFile(w, r, "./error/unauthenticated.html")
+		if r.Method == http.MethodPost {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				log.Printf("Error reading /organization body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+			defer r.Body.Close()
+
+			query, err := url.ParseQuery(string(body))
+			if err != nil {
+				log.Printf("Error parsing query from /organization body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			q1, err := NewQuestion(query.Get("question1"))
+			if err != nil {
+				log.Printf("Error creating question from /feedback body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			q2, err := NewQuestion(query.Get("question2"))
+			if err != nil {
+				log.Printf("Error creating question from /feedback body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			q3, err := NewQuestion(query.Get("question3"))
+			if err != nil {
+				log.Printf("Error creating question from /feedback body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			q4, err := NewQuestion(query.Get("question4"))
+			if err != nil {
+				log.Printf("Error creating question from /feedback body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			q5, err := NewQuestion(query.Get("question5"))
+			if err != nil {
+				log.Printf("Error creating question from /feedback body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			f, err := NewFeedback(query.Get("role"), []Question{q1, q2, q3, q4, q5})
+			if err != nil {
+				log.Printf("Error creating feedback from /feedback body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			log.Printf("/feedback query: %s", f)
+
 			return
 		}
 
-		if _, err := organizations.FindByUserID(cookie.Value); err != nil {
-			log.Printf("Error finding organization for /feedback: %s", err)
-			http.ServeFile(w, r, "./error/index.html")
-			return
-		}
-
-		http.ServeFile(w, r, "./feedback/index.html")
+		log.Printf("Unexpected http Method '%s' for /feedback", r.Method)
+		http.ServeFile(w, r, "./error/index.html")
 	}
 }
--- organization/handler.go
+++ organization/handler.go
@@ -50,77 +50,84 @@ func Handler(organizations *Organizations) http.HandlerFunc {
 			return
 		}
 
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Printf("Error reading /organization body: %s", err)
-			http.ServeFile(w, r, "./error/index.html")
-			return
-		}
-		defer r.Body.Close()
+		if r.Method == http.MethodPost {
+			body, err := io.ReadAll(r.Body)
+			if err != nil {
+				log.Printf("Error reading /organization body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+			defer r.Body.Close()
 
-		query, err := url.ParseQuery(string(body))
-		if err != nil {
-			log.Printf("Error parsing query from /organization body: %s", err)
-			http.ServeFile(w, r, "./error/index.html")
-			return
-		}
+			query, err := url.ParseQuery(string(body))
+			if err != nil {
+				log.Printf("Error parsing query from /organization body: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
 
-		email, err := mail.ParseAddress(query.Get("email"))
-		if err != nil {
-			log.Printf("Error parsing email from /organization email parameter: %s", err)
-			http.ServeFile(w, r, "./error/index.html")
-			return
-		}
+			email, err := mail.ParseAddress(query.Get("email"))
+			if err != nil {
+				log.Printf("Error parsing email from /organization email parameter: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
 
-		parts := strings.Split(email.Address, "@")
-		if len(parts) < 2 {
-			log.Printf("Error parsing email from /organization email parameter: %s", err)
-			http.ServeFile(w, r, "./error/index.html")
-			return
-		}
+			parts := strings.Split(email.Address, "@")
+			if len(parts) < 2 {
+				log.Printf("Error parsing email from /organization email parameter: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
 
-		orgID, err := uuid.NewRandom()
-		if err != nil {
-			log.Printf("Error creating org UUID in /organization: %s", err)
-			http.ServeFile(w, r, "./error/index.html")
-			return
-		}
+			orgID, err := uuid.NewRandom()
+			if err != nil {
+				log.Printf("Error creating org UUID in /organization: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
 
-		userID, err := uuid.NewRandom()
-		if err != nil {
-			log.Printf("Error creating user UUID in /organization: %s", err)
-			http.ServeFile(w, r, "./error/index.html")
-			return
-		}
+			userID, err := uuid.NewRandom()
+			if err != nil {
+				log.Printf("Error creating user UUID in /organization: %s", err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
 
-		org := Organization{
-			ID:     orgID,
-			Domain: parts[1],
-			Users: []User{
-				{
-					ID:    userID,
-					Email: email.Address,
+			org := Organization{
+				ID:     orgID,
+				Domain: parts[1],
+				Users: []User{
+					{
+						ID:    userID,
+						Email: email.Address,
+					},
 				},
-			},
-		}
-		if err := organizations.Add(org); err != nil {
-			log.Printf("Error adding organization '%s' from /organization: %s", org.Domain, err)
-			http.ServeFile(w, r, "./organization/exists.html")
+			}
+			if err := organizations.Add(org); err != nil {
+				log.Printf("Error adding organization '%s' from /organization: %s", org.Domain, err)
+				http.ServeFile(w, r, "./organization/exists.html")
+				return
+			}
+
+			http.SetCookie(w, &http.Cookie{
+				Name:     "__Host-UserUUID",
+				Value:    userID.String(),
+				Path:     "/",
+				Expires:  time.Now().Add(time.Hour * 24 * 31), // One month
+				Secure:   true,
+				HttpOnly: true,
+				SameSite: http.SameSiteStrictMode,
+			})
+
+			log.Printf("New Organization: %s %#v", org.Domain, org)
+
+			http.Redirect(w, r, "/organization/", http.StatusSeeOther)
+
 			return
 		}
 
-		http.SetCookie(w, &http.Cookie{
-			Name:     "__Host-UserUUID",
-			Value:    userID.String(),
-			Path:     "/",
-			Expires:  time.Now().Add(time.Hour * 24 * 31), // One month
-			Secure:   true,
-			HttpOnly: true,
-			SameSite: http.SameSiteStrictMode,
-		})
-
-		log.Printf("New Organization: %s %#v", org.Domain, org)
-
-		http.Redirect(w, r, "/organization/", http.StatusSeeOther)
+		log.Printf("Unexpected http Method '%s' for /organization", r.Method)
+		http.ServeFile(w, r, "./error/index.html")
 	}
 }
