8d6fc2f2a485e77e7082a5924749fa97abe2c3d7
Justin Fuller <justindanielfuller@gmail.com>
2022-10-26 12:40:04 -0400

    Show feedback given

---
+++ feedback/given.go
@@ -0,0 +1,75 @@
+package feedback
+
+import (
+	"log"
+	"net/http"
+	"strings"
+	"text/template"
+
+	"github.com/google/uuid"
+	"github.com/justindfuller/interviews/organization"
+)
+
+func GivenHandler(organizations *organization.Organizations) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("__Host-UserUUID")
+		if err != nil {
+			log.Printf("Error parsing cookie for /feedback/give: %s", err)
+			http.ServeFile(w, r, "./error/unauthenticated.html")
+			return
+		}
+
+		if cookie.Value == "" {
+			log.Printf("Error parsing cookie for /feedback/give: %s", err)
+			http.ServeFile(w, r, "./error/unauthenticated.html")
+			return
+		}
+
+		org, err := organizations.FindByUserID(cookie.Value)
+		if err != nil {
+			log.Printf("Error finding organization for /feedback/give: %s", err)
+			http.ServeFile(w, r, "./error/index.html")
+			return
+		}
+
+		paths := strings.Split(r.URL.Path, "/")
+
+		if len(paths) < 4 || paths[3] == "" {
+			log.Printf("No ID provided for /feedback/give: %s", err)
+			http.ServeFile(w, r, "./error/index.html")
+			return
+		}
+
+		id, err := uuid.Parse(paths[3])
+		if err != nil {
+			log.Printf("Error parsing ID for /feedback/give/{id}: %s", err)
+			http.ServeFile(w, r, "./error/index.html")
+			return
+		}
+
+		f, err := org.FeedbackByID(id)
+		if err != nil {
+			log.Printf("Error finding feedback for  /feedback/give/%s: %s", id, err)
+			http.ServeFile(w, r, "./error/index.html")
+			return
+		}
+
+		if r.Method == http.MethodGet {
+			t, err := template.ParseFiles("./feedback/given.html")
+			if err != nil {
+				log.Printf("Error parsing template for /feedback/give/%s: %s", id, err)
+				http.ServeFile(w, r, "./error/index.html")
+				return
+			}
+
+			if err := t.Execute(w, f); err != nil {
+				log.Printf("Error executing template for /feedback/give/%s: %s", id, err)
+			}
+
+			return
+		}
+
+		log.Printf("Unexpected http Method '%s' for /feedback/give", r.Method)
+		http.ServeFile(w, r, "./error/index.html")
+	}
+}
