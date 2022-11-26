d5b3c310456916a67244dd27df5b747e8be16d51
Justin Fuller <justindanielfuller@gmail.com>
2022-11-25 21:52:44 -0500

    Persist data in app engine

--- cmd/appengine/main.go
+++ cmd/appengine/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
+	"time"
 
+	"cloud.google.com/go/storage"
+	interview "github.com/justindfuller/interviews"
 	"github.com/justindfuller/interviews/api"
 )
 
@@ -28,6 +34,52 @@ func main() {
 			log.Fatal(err)
 		}
 	}
+
+	ctx := context.Background()
+
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatalf("Error creating storage client: %s", err)
+	}
+
+	object := client.Bucket("betterinterviews").Object("organizations.json")
+
+	rc, err := object.NewReader(ctx)
+	if err != nil {
+		log.Fatalf("Error creating reader for storage bucket: %s", err)
+	}
+	defer rc.Close()
+
+	body, err := io.ReadAll(rc)
+	if err != nil {
+		log.Fatalf("Error reading data from storage bucket: %s", err)
+	}
+
+	if err := json.Unmarshal(body, &interview.DefaultOrganizations); err != nil {
+		log.Fatalf("Error decoding data to DefaultOrganizations: %s", err)
+	}
+
+	go func() {
+		write := func() {
+			file := object.NewWriter(ctx)
+			defer file.Close()
+
+			interview.DefaultOrganizations.Mutex.Lock()
+			defer interview.DefaultOrganizations.Mutex.Unlock()
+
+			enc := json.NewEncoder(file)
+			enc.SetIndent("", "  ")
+			if err := enc.Encode(&interview.DefaultOrganizations); err != nil {
+				log.Printf("Error encoding to gob: %s", err)
+				return
+			}
+		}
+
+		write()
+		for _ = range time.Tick(30 * time.Second) {
+			write()
+		}
+	}()
 
 	api.Handlers()
 
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/justindfuller/interviews
 go 1.15
 
 require (
+	cloud.google.com/go/storage v1.28.0
 	github.com/google/uuid v1.3.0
 	github.com/pkg/errors v0.9.1
 )
