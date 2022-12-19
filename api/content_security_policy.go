1de1787af559a4edb3d09b9bd34cb401d59cb5c3
Justin Fuller <justindanielfuller@gmail.com>
2022-12-19 10:02:10 -0500

    Remove redirect for naked domain

--- api/content_security_policy.go
+++ api/content_security_policy.go
@@ -4,11 +4,6 @@ import "net/http"
 
 func withContentSecurityPolicy(handler func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Hostname() == "betterinterviews.com" {
-			http.Redirect(w, r, "https://www.betterinterviews.com"+r.URL.Path, http.StatusMovedPermanently)
-			return
-		}
-
 		w.Header().Set("Content-Security-Policy", "default-src 'none'; connect-src 'self'; img-src 'self'; style-src 'unsafe-inline'; form-action 'self'; manifest-src 'self'; script-src 'sha256-g1OdTc+cgep4LHYKLdcx6nMjok9omWedJvE365v/NGE='; worker-src 'self'; report-uri /csp-violation/")
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
